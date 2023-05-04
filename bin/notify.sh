5d431d62380cdfc63b26204bd0ddde430333b902
Miroslav Blasko <blcham@gmail.com>
2023-05-04 12:46:29 +0200

    [Upd] Additional help related to configuration

--- bin/notify.sh
+++ bin/notify.sh
@@ -2,9 +2,20 @@
 
 # Helper script to send messages to slack channel.
 
-# Webhook to slack channel typically starting with https://hooks.slack.com/services/
+#################
+# CONFIGURATION #
+#################
+
+# webhook to slack channel typically starting with https://hooks.slack.com/services/
 WEBHOOK=
+
+# context of assembly line e.g. PROD/DEV
 CTX=PROD
+
+########
+# MAIN #
+########
+
 MESSAGE=$1
 ICON=$2
 
